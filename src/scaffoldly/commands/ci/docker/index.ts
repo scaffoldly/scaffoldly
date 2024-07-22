6330321e15052f349048b1e8574fd41532b8053e
Christian Nuss <christian.nuss@gmail.com>
2024-07-22 08:49:20 -0400

    add join back on src

--- src/scaffoldly/commands/ci/docker/index.ts
+++ src/scaffoldly/commands/ci/docker/index.ts
@@ -320,8 +320,8 @@ export class DockerService {
             if (from && f) {
               copy.push({
                 from: `${mode}-${from}`,
-                src: join(workdir, f),
-                dest: join(workdir, f),
+                src: join(workdir, join(src, f)),
+                dest: join(workdir, join(src, f)),
               });
               return;
             }
@@ -363,8 +363,8 @@ export class DockerService {
           if (from && f) {
             copy.push({
               from: `${mode}-${from}`,
-              src: join(workdir, f),
-              dest: join(workdir, f),
+              src: join(workdir, join(src, f)),
+              dest: join(workdir, join(src, f)),
             });
             return;
           }
@@ -387,8 +387,8 @@ export class DockerService {
             if (from && f) {
               const cp: Copy = {
                 from: `${mode}-${from}`,
-                src: join(workdir, f),
-                dest: join(workdir, f),
+                src: join(workdir, join(src, f)),
+                dest: join(workdir, join(src, f)),
               };
               return cp;
             }
