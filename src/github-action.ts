66cd483c7087c099d05f0378fc0efaed04ed3f8b
Christian Nuss <christian.nuss@gmail.com>
2024-09-01 19:46:25 -0400

    refactor entrypoint

--- src/github-action.ts
+++ src/github-action.ts
@@ -1,20 +1,27 @@
 import { Action, Mode } from './github-action/action';
 import { State } from './github-action/state';
-import { saveState, getState, debug, setOutput, summary, error } from '@actions/core';
+import {
+  saveState,
+  getState,
+  debug,
+  // setOutput,
+  summary,
+  error,
+  info,
+} from '@actions/core';
 
 process.on('unhandledRejection', (reason, promise) => {
   console.warn('Unhandled Rejection at:', promise, 'reason:', reason);
-  process.exit(2);
+  process.exit(-1);
 });
 
 process.on('uncaughtException', (err) => {
   console.error('Uncaught Exception:', err);
-  process.exit(3);
+  process.exit(-1);
 });
 
 export const run = async (mode: Mode): Promise<void> => {
   const action = await new Action(mode).init();
-
   let state: State = { status: {} };
 
   try {
@@ -34,21 +41,29 @@ export const run = async (mode: Mode): Promise<void> => {
 
     debug('New state: ' + JSON.stringify(state));
 
-    setOutput('TODO', 'TODO');
-
     if (mode === 'post' && state.longMessage) {
       summary.addRaw(state.longMessage, true);
       await summary.write({ overwrite: true });
     }
 
     saveState('state', JSON.stringify(state));
+    // setOutput('TODO', 'TODO');
 
     if (state.failed) {
-      throw new Error(`${mode} step failed`);
+      throw new Error(`${mode} step failed: ${state.shortMessage}`);
     }
   } catch (e) {
-    error(e);
-    debug(e.stack);
-    process.exit(1);
+    if (e instanceof Error) {
+      debug(`${e}`);
+      if (e.cause && e.cause instanceof Error) {
+        error(`Error: ${e.message}:\n  ${e.cause.message}`);
+      } else {
+        error(`Error: ${e.message}`);
+      }
+      info(`\nRe-run the action with debug enabled for more information.`);
+    } else {
+      error(`${e}`);
+    }
+    process.exit(-1);
   }
 };
