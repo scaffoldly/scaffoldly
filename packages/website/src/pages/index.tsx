7ef1dd535bb9af3e798d4cfc510234e8a5c5f67d
Christian Nuss <christian.nuss@gmail.com>
2024-09-11 18:26:23 -0700

    fix homepage link

--- packages/website/src/pages/index.tsx
+++ packages/website/src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/docs/tutorial">
+          <Link className="button button--secondary button--lg" to="/docs/tutorials/nextjs">
             NextJS on AWS Lambda Tutorial - 5min ⏱️
           </Link>
         </div>
